9c8b425540dc1eaa8fa6280496de435db1df1375
ZENOXX <zenoxxreturns@gmail.com>
2026-01-14 14:29:24 +0530

    Add files via upload

---
+++ utils/config.py
@@ -0,0 +1,13 @@
+import os
+
+TOKEN = os.environ.get("TOKEN")
+NAME = "Meekly"
+server = "https://discord.com/invite/codexdev"
+ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
+OWNER_IDS = [767979794411028491, 912362112620331029, 1297508239029698695, 1010057368287068222]
+BotName = "Meekly"
+serverLink = "https://discord.com/invite/codexdev"
+
+API_KEY = os.environ.get("API_KEY")
+API_PORT = int(os.environ.get("API_PORT", 8080))
+DASHBOARD_URL = os.environ.get("DASHBOARD_URL", "http://localhost:3000")
